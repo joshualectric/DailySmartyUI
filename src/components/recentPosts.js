39b0af17395d3e05c63a77d88ebd429f4953bdbd
joshualectric <joshuainrecovery7@gmail.com>
2021-07-29 17:55:31 -0500

    added mapStateToProps in the recentPosts.js component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -11,6 +11,19 @@ import * as actions from '../actions';
         this.props.fetchRecentPosts();
     }
 
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            
+            if(index < 3) {
+                return (
+                    <li key={index}>
+                        {post.title}
+                    </li>
+                )
+            }        
+        })
+        return posts
+    }
 
     render() {
         return (
@@ -18,10 +31,7 @@ import * as actions from '../actions';
                 <div className="recent-posts__wrapper">
                     <div className="recent-posts__heading">Recent Posts</div>
                     <ul className="recent-posts__posts">
-                        <li>recent post 0</li>
-                        <li>recent post 1</li>
-                        <li>recent post 2</li>
-
+                        {this.renderPosts()}
                     </ul>
 
                 </div>
@@ -31,4 +41,10 @@ import * as actions from '../actions';
     }
  }
 
- export default connect(null, actions)(RecentPosts);
+ function mapStateToProps(state) {
+     return {
+         recentPosts: state.posts.recentPosts
+     }
+ }
+
+ export default connect(mapStateToProps, actions)(RecentPosts);
