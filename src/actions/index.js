f766d5145dad88b81898894728d866b903eb786d
joshualectric <joshuainrecovery7@gmail.com>
2021-08-05 14:33:23 -0500

    added SET_RESULTS_POSTS reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { SET_RECENT_POSTS } from './types';
+import { SET_RECENT_POSTS,
+         SET_RESULTS_POSTS
+} from './types';
+
 import axios from 'axios';
 
 export function fetchRecentPosts() {
@@ -20,7 +23,7 @@ export function fetchPostsWithQuery(query) {
             .then(response => {
                 console.log(response.data.posts)
                 dispatch({
-                    type: SET_RECENT_POSTS,
+                    type: SET_RESULTS_POSTS,
                     payload: response.data.posts
                 })
             })
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,10 +1,11 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
 import recentPosts from '../components/recentPosts';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -15,6 +16,12 @@ export default function(state = INIT_STATE, action) {
             return {
                     ...state, 
                     recentPosts
+                }
+            case SET_RESULTSPOSTS:
+                const resultsPosts = action.payload;
+                return {
+                    ...state,
+                    resultsPosts
                 }
     
         default:
