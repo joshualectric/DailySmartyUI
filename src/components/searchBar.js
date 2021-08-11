7db506319629d81f90ff1390acb51fc3b19e20d0
joshualectric <joshuainrecovery7@gmail.com>
2021-08-11 17:44:33 -0500

    implemented font awesome styles into searchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -10,7 +10,7 @@ import { withRouter } from 'react-router-dom';
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
     }
 
     render() {
